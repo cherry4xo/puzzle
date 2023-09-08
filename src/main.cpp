126df77c9c805f4ebc35098ad7c478fa78752a45
cherry4xo <1jlaketuk1@gmail.com>
2023-09-08 10:27:07 +0300

    upd: pattern

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include "pattern.hpp"
 
 int main()
 {
--- src/pattern.hpp
+++ src/pattern.hpp
@@ -7,6 +7,13 @@
 #include <sys/types.h>
 #include <tuple>
 #include <math.h>
+#include <cmath>
+#include <vector>   
+
+inline float angle(sf::Vector2f v)
+{
+    return std::atan2(v.x, v.y);
+}
 
 class ShapeStrategy : public sf::Shape
 {
@@ -17,9 +24,11 @@ public:
     virtual void set_point_count(const u_int16_t point_count);
     virtual void set_size(const sf::Vector2f&) = 0;
     virtual const sf::Vector2f& get_size() = 0;
-    virtual sf::Vector2f get_point(u_int16_t) const;
+    virtual sf::Vector2f get_point(u_int16_t) const = 0;
+    virtual void update_points() = 0;
 protected:
     u_int16_t point_count_;
+    std::vector<sf::Vector2f> points_;
 };
 
 class EllipceStrategy : public ShapeStrategy
@@ -30,23 +39,44 @@ public:
     void set_size(const sf::Vector2f& radius) override;
     const sf::Vector2f& get_size() override;
     sf::Vector2f get_point(u_int16_t index) const override;
+    virtual void update_points() override;
 protected:
     sf::Vector2f radius_;
+};
+
+class TopEllipceStrategy : public EllipceStrategy
+{
+
+};
+
+class BottomEllipceStrategy : public EllipceStrategy
+{
+
+};
+
+class RightEllipceStrategy : public EllipceStrategy
+{
+
+};
+
+class LeftEllipceStrategy : public EllipceStrategy
+{
+
 };
 
 class PuzzleSide
 {
 public:
     PuzzleSide(ShapeStrategy* s);
-    ~PuzzleSide();
+    virtual ~PuzzleSide() = default;
     void set_shape(ShapeStrategy* s);
     void set_point_count(u_int16_t point_count);
     u_int16_t get_point_count();
     void set_size(const sf::Vector2f& radius);
     const sf::Vector2f& get_size();
     virtual sf::Vector2f get_point(u_int16_t index) const;
-
-private:
+    virtual void update_points();
+protected:
     ShapeStrategy* shapeStrategy_ = nullptr;
 };
 
